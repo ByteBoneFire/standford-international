d09b53d39e9411edc48509adcf97bd57df58b344
ByteBoneFire <sarojbc040@gmail.com>
2025-02-17 17:12:11 +0545

    Complete the teams Sections

---
+++ src/utils/teamsDetails.jsx
@@ -0,0 +1,34 @@
+const TeamsDetails = [
+    {
+      name: 'Kim Berly Hansen',
+      title: 'CEO & Co-Founder',
+      image: 'assets/Teams/Team1.jpeg'
+    },
+    {
+      name: 'Berly Kim Kardian',
+      title: 'COO & Co-Founder',
+      image: 'assets/Teams/Team2.jpg'
+    },
+    {
+      name: 'Berly Yum Kardian',
+      title: 'Senior Consultant',
+      image: 'assets/Teams/Team3.jpg'
+    },
+    {
+      name: 'John Doe',
+      title: 'Consutling',
+      image: 'assets/Teams/Team3.jpg'
+    },
+    {
+      name: 'Jane Smith',
+      title: 'COO & Marketing Officer',
+      image: 'assets/Teams/Team2.jpg'
+    },
+    {
+      name: 'Mike Johnson',
+      title: 'Marketing Manager',
+      image: 'assets/Teams/Team1.jpeg'
+    },
+  ];
+
+  export default TeamsDetails;
